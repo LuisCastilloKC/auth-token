8c5c9b1f0e28986411569cb7d0ed93629dd2679c
Luis Castillo <mr.luiscastillo@gmail.com>
2022-08-05 20:40:56 -0400

    add users route

--- auth-backend/config/routes.rb
+++ auth-backend/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:create, :index]
+  post '/login', to: 'users#login'
+  get '/auto_login', to: 'users#auto_login'
+
 end
