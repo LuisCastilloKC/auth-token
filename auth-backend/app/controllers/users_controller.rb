25c4d8f090ef0a34279a9715487f89d4b515241a
Luis Castillo <mr.luiscastillo@gmail.com>
2022-08-05 20:52:09 -0400

    add user private and user params

--- auth-backend/app/controllers/users_controller.rb
+++ auth-backend/app/controllers/users_controller.rb
@@ -16,4 +16,10 @@ class UsersController < ApplicationController
         end
     end
 
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+
 end
