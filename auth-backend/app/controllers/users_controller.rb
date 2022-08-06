8072941fed34c6e02c9fc7485819f93a81b5cbfc
Luis Castillo <mr.luiscastillo@gmail.com>
2022-08-05 20:49:43 -0400

    def create method for user

--- auth-backend/app/controllers/users_controller.rb
+++ auth-backend/app/controllers/users_controller.rb
@@ -5,4 +5,15 @@ class UsersController < ApplicationController
         render json: user
     end
 
+    def create 
+        user = User.create(user_params)
+        if user.valid?
+            payload = {user_id: user.id}
+            token = encode_token(payload)
+            render json: {user: user, jwt: token}, status: :created
+        else
+            render json: {errors: user.errors.full_messages}
+        end
+    end
+
 end
